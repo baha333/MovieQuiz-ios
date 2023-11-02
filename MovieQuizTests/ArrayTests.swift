334109cb38f20dec04f06c71efff46ccd9bdc0af
Bakha <109457622+baha333@users.noreply.github.com>
2023-11-02 14:16:32 +0300

    Add files via upload

---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,35 @@
+//
+//  ArrayTests.swift
+//  MovieQuizTests
+//
+//  Created by Bakhadir on 17.10.2023.
+//
+
+import Foundation
+import XCTest // Фреймворк для тестирования
+@testable import MovieQuiz // Импортируем наше приложение для тестирования
+
+class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws { // тест на успешное взятие элемента по индексу
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 2]
+        
+        // Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func testGetValueOutOfRange() throws { // тест на взятие элемента по неправильному индексу
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 20]
+        
+        // Then
+        XCTAssertNil(value)
+    }
+}
